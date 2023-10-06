89a21928a0d926d6dfcefe802e9cb90f6a6427f5
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-10-06 03:02:33 +0100

    Add support for Act 1 and 2

--- src/modules/quests.js
+++ src/modules/quests.js
@@ -1,29 +1,29 @@
 import questData from './questData.json';
 
-const actThreeAreas = questData.acts[0].areas;
-
 export default function initialise() {
-  for (const area of actThreeAreas) {
-    const ul = document.querySelector(`section[data-area="${area.name}"] > ul`);
+  const actButtons = document.querySelectorAll('.act-button');
+  actButtons.forEach((btn) =>
+    btn.addEventListener('click', (e) => loadAct(Number(e.target.dataset.act))),
+  );
 
-    for (const quest of area.quests) {
-      const checkbox = createQuestCheckbox(quest);
-      ul.appendChild(checkbox);
-
-      if (quest.subquests) {
-        for (const subquest of quest.subquests) {
-          const subCheckbox = createQuestCheckbox(subquest);
-          subCheckbox.classList.add('subquest');
-          ul.appendChild(subCheckbox);
-        }
-      }
-    }
-  }
-  loadCheckboxStates();
+  loadAct(1);
 }
 
-function loadCheckboxStates() {
-  for (const area of actThreeAreas) {
+function loadAct(actNumber) {
+  const locContainer = document.querySelector('.sublocation-container');
+  locContainer.classList = `sublocation-container sublocation-container--act${actNumber}`;
+
+  const actButtons = document.querySelectorAll('.act-button');
+  actButtons.forEach((btn) => btn.classList.remove('active'));
+
+  const actButton = document.querySelector(`.act-button[data-act="${actNumber}"]`);
+  actButton.classList.add('active');
+
+  loadQuests(questData.acts[actNumber - 1].areas);
+}
+
+function loadCheckboxStates(areaList) {
+  for (const area of areaList) {
     for (const quest of area.quests) {
       const checkbox = document.getElementById(quest.id);
       const savedState = localStorage.getItem(`quest_${quest.id}`);
@@ -46,6 +46,37 @@ function loadCheckboxStates() {
   }
 }
 
+function loadQuests(areaList) {
+  const locContainer = document.querySelector('.sublocation-container');
+  locContainer.innerHTML = '';
+
+  for (const area of areaList) {
+    const section = document.createElement('section');
+    const h1 = document.createElement('h1');
+    const ul = document.createElement('ul');
+
+    section.classList.add('area-card');
+    h1.innerText = area.name;
+    section.appendChild(h1);
+    section.appendChild(ul);
+    locContainer.appendChild(section);
+
+    for (const quest of area.quests) {
+      const checkbox = createQuestCheckbox(quest);
+      ul.appendChild(checkbox);
+
+      if (quest.subquests) {
+        for (const subquest of quest.subquests) {
+          const subCheckbox = createQuestCheckbox(subquest);
+          subCheckbox.classList.add('subquest');
+          ul.appendChild(subCheckbox);
+        }
+      }
+    }
+  }
+  loadCheckboxStates(areaList);
+}
+
 function saveCheckboxState(e) {
   localStorage.setItem(`quest_${e.target.id}`, e.target.checked);
 }
@@ -59,16 +90,24 @@ function createQuestCheckbox(quest) {
 
   input.addEventListener('change', (e) => saveCheckboxState(e));
 
-  const a = document.createElement('a');
-  a.href = quest.url;
-  a.target = '_blank';
-  a.textContent = quest.name;
-
   const label = document.createElement('label');
   label.classList = 'label';
   label.htmlFor = quest.id;
   label.appendChild(input);
-  label.appendChild(a);
+
+  if (quest.url) {
+    const a = document.createElement('a');
+    a.href = quest.url;
+    a.target = '_blank';
+    a.textContent = quest.name;
+
+    label.appendChild(a);
+  } else {
+    const span = document.createElement('span');
+    span.textContent = quest.name;
+
+    label.appendChild(span);
+  }
 
   const li = document.createElement('li');
   li.appendChild(label);
